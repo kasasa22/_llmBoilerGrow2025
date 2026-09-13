13fad864690521adf42f44461bfd0584cbff8a1f
kasasa22 <kasasatrevor25@gmail.com>
2026-09-13 17:05:24 +0300

    fix(infra): update civo provider version constraint to allow minor updates

--- infra/tf/providers.tf
+++ infra/tf/providers.tf
@@ -10,7 +10,7 @@ terraform {
     #  User to provision resources (firewal / cluster) in civo.com
     civo = {
       source  = "civo/civo"
-      version = "1.1.2"
+      version = "~> 1.1"
     }
 
     # Used to output the kubeconfig to the local dir for local cluster access
